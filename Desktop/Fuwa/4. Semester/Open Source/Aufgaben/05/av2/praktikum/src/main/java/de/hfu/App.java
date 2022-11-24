69341285de7f411eb35036a599ee0be9a552356c
Florim Sabani <sabaniflorim@gmail.com>
2022-11-24 12:00:18 +0100

    site

--- Desktop/Fuwa/4. Semester/Open Source/Aufgaben/05/av2/praktikum/src/main/java/de/hfu/App.java
+++ Desktop/Fuwa/4. Semester/Open Source/Aufgaben/05/av2/praktikum/src/main/java/de/hfu/App.java
@@ -2,8 +2,19 @@ package de.hfu;
 
 import java.util.Scanner;
 
+/**Main class
+ * 
+ * @author Florim
+ * @version 1.0
+ *
+ */
 public class App 
 {
+	/**Main method, takes user input and prints the input in upper case
+	 * 
+	 * 
+	 * @param args
+	 */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter your text: ");
